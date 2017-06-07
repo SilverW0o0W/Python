7ba6693c6fe871cff8d1bf400957900cef9dd998
Silver <silver.codingcat@gmail.com>
2017-06-07 15:53:09 +0800

    Fibonacci sequence

---
+++ Code/fibonacci_sequence.py
@@ -0,0 +1,18 @@
+"""
+This is Fibonacci sequence
+"""
+
+def fib(max_number):
+    """This is Fibonacci sequence"""
+    number, before, after = 0, 0, 1
+    while number < max_number:
+        yield after
+        # temp = after
+        # after = before + after
+        # before = temp
+        before, after = after, before + after
+        number = number + 1
+    return 'done'
+
+for n in fib(5):
+    print(n)
