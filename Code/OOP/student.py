ba8a0771be9749f51442559a4f90ddd4614fa9a4
Silver <silver.codingcat@gmail.com>
2017-06-11 19:53:09 +0800

    add student prop method

--- Code/OOP/student.py
+++ Code/OOP/student.py
@@ -6,8 +6,20 @@ from person import Person
 class Student(Person):
     """Student : school grade class id"""
 
-    def __init__(self, name, gender, age, id, grade, classNumber):
+    def __init__(self, name, gender, age, id, grade, class_number):
         Person.__init__(name, gender, age)
         self.__id = id
         self.__grade = grade
-        self.__class = classNumber
+        self.__class = class_number
+
+    def get_id(self):
+        """Get id value"""
+        return self.__id
+
+    def get_grade(self):
+        """Get grade value"""
+        return self.__grade
+
+    def get_class(self):
+        """Get class value"""
+        return self.__class
