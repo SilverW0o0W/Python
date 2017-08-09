db6c83db29c99cc0f78e4043cb070ac548b94929
Silver <silver.codingcat@gmail.com>
2017-08-09 21:22:46 +0800

    Add simple proxy check code
    Next step for multithreading check

--- Code/netease-music/proxy_spider.py
+++ Code/netease-music/proxy_spider.py
@@ -5,13 +5,31 @@ This is for proxy server, avoid to ban the own ip.
 
 import urllib2
 
-proxy_handler = urllib2.ProxyHandler({'http': '111.155.116.219:8123'})
-opener = urllib2.build_opener(proxy_handler)
-r = opener.open('http://www.silvercodingcat.com')
-print r.read()
-print 'go'
+
+class ProxySpider(object):
+
+    __check_http_url = 'http://silvercodingcat.com/python/2017/08/09/Proxy-Spider-Check/'
+    __check_https_url = ''
+
+    def check_proxy(self, is_http, ip_port):
+        """
+        Check proxy available
+        """
+
+        transfer_method = 'http' if is_http else 'https'
+        proxy_handler = urllib2.ProxyHandler({transfer_method: ip_port})
+        opener = urllib2.build_opener(proxy_handler)
+        response = opener.open(self.__check_http_url)
+        if response == 200:
+            return True
+        else:
+            return False
 # 也可以用install_opener将配置好的opener安装到全局环境中，这样所有的urllib2.urlopen都会自动使用代理。
 
-urllib2.install_opener(opener)
-r = urllib2.urlopen('http://www.silvercodingcat.com')
-print r.read()
+# urllib2.install_opener(opener)
+# r = urllib2.urlopen('http://www.silvercodingcat.com')
+# print r.read()
+
+
+spider = ProxySpider()
+spider.check_proxy(True, '221.201.81.248:80')
