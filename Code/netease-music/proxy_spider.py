6b592e61b57ca4df898855e450b9bb0ca2c6ef62
Silver <silver.codingcat@gmail.com>
2017-08-10 20:50:40 +0800

    Add db for saving proxy ip(incomplete)

--- Code/netease-music/proxy_spider.py
+++ Code/netease-music/proxy_spider.py
@@ -6,6 +6,10 @@ This is for proxy server, avoid to ban the own ip.
 import threading
 import urllib2
 
+import os
+import sqlite3
+
+
 class ProxySpider(object):
     """
     This is a class for crawling proxy ip
@@ -14,13 +18,18 @@ class ProxySpider(object):
     __check_https_url = ''
     __thread_result = threading.local()
 
+    __db_path = 'proxy_ip.db'
+    __db_conn = None
+    __db_sql_create_table = 'create table proxy_ip(id INT primary key, ip VARCHAR(20), port VARCHAR(10),method TINYINT,available TINYINT)'
+
     def send_check_request(self, proxy_data, check_url):
         """
         Send check request. Timeout: 10s. Retry: 3 times.
         """
         for i in range(3):
-            check_thread = threading.Thread(target=self.send_check_request_thread, args=(proxy_data, check_url,))
-            print 'retry'+ str(i)
+            check_thread = threading.Thread(
+                target=self.send_check_request_thread, args=(proxy_data, check_url,))
+            print 'retry' + str(i)
             check_thread.start()
             check_thread.join(10)
             if check_thread.is_alive():
@@ -41,14 +50,44 @@ class ProxySpider(object):
         except urllib2.URLError:
             self.__thread_result = False
 
-    def check_proxy(self, is_http, ip_port):
+    def check_proxy(self, is_https, ip_port):
         """
         Check proxy available
         """
-        transfer_method = 'http' if is_http else 'https'
+        transfer_method = 'https' if is_https else 'http'
         proxy_data = {transfer_method: ip_port}
-        check_url = self.__check_http_url if is_http else self.__check_https_url
+        check_url = self.__check_http_url if is_https else self.__check_https_url
         return self.send_check_request(proxy_data, check_url)
 
+    def establish_db_onnection(self):
+        """
+        Establish sqlite connection. If it don't exist, create the db file.
+        Return: The file is existed before exstablish connection.
+        """
+        is_exist = os.path.exists(self.__db_path)
+        self.__db_conn = sqlite3.connect(self.__db_path)
+        return is_exist
+
+    def create_db_table(self, sql=__db_sql_create_table):
+        """
+        Create the proxy ip table.
+        """
+        cursor = self.__db_conn.cursor()
+        cursor.execute(sql)
+        cursor.close()
+
+
+class ProxyIP(object):
+    """
+    This is the class for ip information.
+    """
+
+    def __init__(self, ip='', port='', is_https=False, available=False):
+        self.__ip = ip
+        self.__port = port
+        self.__is_https = is_https
+        self.__available = available
+
+
 spider = ProxySpider()
-print spider.check_proxy(True, '221.201.81.248:80')
+print spider.check_proxy(False, '221.201.81.248:80')
