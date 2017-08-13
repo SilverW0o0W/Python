d3c8db71da60ef51d4872b71dbd31a0f76e44f4f
Silver <silver.codingcat@gmail.com>
2017-08-13 23:07:32 +0800

    Insert proxy to db complete
    Next step crawling ip page

--- Code/netease-music/proxy_spider.py
+++ Code/netease-music/proxy_spider.py
@@ -16,12 +16,13 @@ class ProxySpider(object):
     """
     __check_http_url = 'http://silvercodingcat.com/python/2017/08/09/Proxy-Spider-Check/'
     __check_https_url = ''
+    __thread_timeout = 15
     __thread_result = threading.local()
 
     __db_path = 'proxy_ip.db'
     __db_conn = None
-    __sql_create_table = 'create table proxy_ip(id INT primary key autoincrement, ip VARCHAR(20), port VARCHAR(10),https TINYINT,available TINYINT)'
-    __sql_insert_ip = 'insert into proxy_ip values(?, ?, ?, ?)'
+    __sql_create_table = 'create table proxy_ip(id INTEGER primary key autoincrement, ip VARCHAR(20), port VARCHAR(10),https TINYINT,available TINYINT)'
+    __sql_insert_ip = 'insert into proxy_ip values(null, ?, ?, ?, ?)'
     __sql_select_ip_exist = 'select * from proxy_ip where ip = ? and port = ?'
 
     def __init__(self):
@@ -29,14 +30,14 @@ class ProxySpider(object):
 
     def send_check_request(self, proxy_data, check_url):
         """
-        Send check request. Timeout: 10s. Retry: 3 times.
+        Send check request. Timeout: 15s. Retry: 3 times.
         """
         for i in range(3):
             check_thread = threading.Thread(
                 target=self.send_check_request_thread, args=(proxy_data, check_url,))
             print 'retry' + str(i)
             check_thread.start()
-            check_thread.join(10)
+            check_thread.join(self.__thread_timeout)
             if check_thread.is_alive():
                 continue
             else:
@@ -52,8 +53,12 @@ class ProxySpider(object):
         try:
             response = opener.open(url)
             self.__thread_result = response.code == 200
-        except urllib2.URLError:
+        except urllib2.HTTPError, error:
             self.__thread_result = False
+            print error
+        except urllib2.URLError, error:
+            self.__thread_result = False
+            print error
 
     def check_proxy(self, proxy_ip):
         """
@@ -73,7 +78,7 @@ class ProxySpider(object):
         sql = self.__sql_insert_ip
         params_list = (proxy_ip.ip, proxy_ip.port,
                        1 if proxy_ip.is_https else 0, 1 if proxy_ip.available else 0)
-        return self.sql_execute(sql, params_list)
+        return self.sql_write(sql, params_list)
 
     def add_proxy(self, proxy_ip):
         """
@@ -81,16 +86,17 @@ class ProxySpider(object):
         """
         if self.check_proxy(proxy_ip):
             if self.check_proxy_exist(proxy_ip):
-                return
+                return False
             else:
                 self.insert_proxy_db(proxy_ip)
+                return True
 
     def check_proxy_exist(self, proxy_ip):
         """
         Check proxy existed in sqlite
         """
-        params_list = (proxy_ip.ip, proxy_ip.port)
-        result_set = self.sql_execute(self.__sql_select_ip_exist, params_list)
+        params_list = (proxy_ip.ip, proxy_ip.port,)
+        result_set = self.sql_read(self.__sql_select_ip_exist, params_list)
         return result_set != None and len(result_set) > 0
 
     def init_db(self):
@@ -101,7 +107,7 @@ class ProxySpider(object):
             db_exist = self.establish_db_connection()
             if not db_exist:
                 # Create db table
-                return self.sql_execute(self.__sql_create_table)
+                return self.sql_write(self.__sql_create_table)
             return True
         except sqlite3.DatabaseError:
             return False
@@ -123,17 +129,37 @@ class ProxySpider(object):
             self.__db_conn.close()
             self.__db_conn = None
 
-    def sql_execute(self, sql, params_list=None):
+    def sql_write(self, sql, params_list=None):
         """
-        Execute sqlite sql.
+        Execute sqlite sql. For write.
         """
         cursor = None
         try:
             cursor = self.__db_conn.cursor()
             if params_list is None:
-                return cursor.execute(sql)
+                result = cursor.execute(sql)
             else:
-                return cursor.execute(sql, params_list)
+                result = cursor.execute(sql, params_list)
+            self.__db_conn.commit()
+            return result
+        except sqlite3.DatabaseError:
+            return None
+        finally:
+            cursor.close()
+
+    def sql_read(self, sql, params_list=None):
+        """
+        Execute sqlite sql. For read.
+        """
+        cursor = None
+        try:
+            cursor = self.__db_conn.cursor()
+            if params_list is None:
+                result = cursor.execute(sql)
+            else:
+                result = cursor.execute(sql, params_list)
+            result_set = result.fetchall()
+            return result_set
         except sqlite3.DatabaseError:
             return None
         finally:
@@ -159,5 +185,5 @@ class ProxyIP(object):
 
 
 spider = ProxySpider()
-proxy = ProxyIP('221.201.81.248', '80', False, False)
+proxy = ProxyIP('175.165.109.207', '8118', False, False)
 print spider.add_proxy(proxy)
