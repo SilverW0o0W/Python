4f7f52b025eb1b7c5ea0c03d2dff5d529d2c97d5
Silver <silver.codingcat@gmail.com>
2017-08-23 22:50:58 +0800

    Re revert mistake

---
+++ Code/netease-music/sqlite_controller.py
@@ -0,0 +1,144 @@
+# coding=utf-8
+"""
+This is for controlling sqlite
+"""
+
+import threading
+
+import os
+import sqlite3
+
+class SqliteController(object):
+    """
+    This is a class for controlling sqlite
+    """
+
+    __sql_create_table = 'create table proxy_ip(id INTEGER primary key autoincrement, ip VARCHAR(20), port VARCHAR(10),https TINYINT,available TINYINT)'
+
+    __db_path = 'proxy_ip.db'
+    __db_connection = None
+    __db_min_storage = 10
+    __db_thread_connection = threading.local()
+
+    def __init__(self):
+        self.init_db()
+
+    def init_db(self, is_main_thread=True):
+        """
+        Initialize sqlite db.
+        """
+        try:
+            db_exist = self.establish_db_connection(is_main_thread)
+            if not db_exist:
+                # Create db table
+                return self.sql_write(self.__sql_create_table)
+            return True
+        except sqlite3.DatabaseError:
+            return False
+
+    def establish_db_connection(self, is_main_thread=True):
+        """
+        Establish sqlite connection. If it don't exist, create the db file.
+        Return: The file is existed before exstablish connection.
+        """
+        is_exist = os.path.exists(self.__db_path)
+        if is_main_thread:
+            self.__db_connection = sqlite3.connect(self.__db_path)
+        else:
+            self.__db_thread_connection = sqlite3.connect(self.__db_path)
+        return is_exist
+
+    def dispose_db_connection(self):
+        """
+        Close db connection
+        """
+        if self.__db_connection != None:
+            self.__db_connection.close()
+            self.__db_connection = None
+
+    def sql_write(self, sql, params_list=None, is_main_thread=True):
+        """
+        Execute sqlite sql. For write.
+        """
+        cursor = None
+        connect = None
+        try:
+            if is_main_thread:
+                connect = self.__db_connection
+            else:
+                self.establish_db_connection(False)
+                connect = self.__db_thread_connection
+            cursor = connect.cursor()
+            if params_list is None:
+                result = cursor.execute(sql)
+            else:
+                result = cursor.execute(sql, params_list)
+            connect.commit()
+            return result
+        except sqlite3.DatabaseError, db_error:
+            print db_error.message
+            return None
+        finally:
+            if cursor is not None:
+                cursor.close()
+            if is_main_thread is False and connect is not None:
+                connect.close()
+
+    def sql_write_list(self, sql, params_list, is_main_thread=True):
+        """
+        Execute sqlite sql. For write.
+        """
+        connect = None
+        cursor = None
+        row_num = 0
+        try:
+            if is_main_thread:
+                connect = self.__db_connection
+            else:
+                self.establish_db_connection(False)
+                connect = self.__db_thread_connection
+            cursor = connect.cursor()
+            for params in params_list:
+                try:
+                    result = cursor.execute(sql, params)
+                    row_num += result.rowcount
+                except sqlite3.DatabaseError:
+                    continue
+            connect.commit()
+            return row_num
+        except sqlite3.DatabaseError, db_error:
+            print db_error.message
+            return None
+        finally:
+            if cursor is not None:
+                cursor.close()
+            if is_main_thread is False and connect is not None:
+                connect.close()
+
+    def sql_read(self, sql, params_list=None, is_main_thread=True):
+        """
+        Execute sqlite sql. For read.
+        """
+        connect = None
+        cursor = None
+        try:
+            if is_main_thread:
+                connect = self.__db_connection
+            else:
+                self.establish_db_connection(False)
+                connect = self.__db_thread_connection
+            cursor = connect.cursor()
+            if params_list is None:
+                cursor.execute(sql)
+            else:
+                cursor.execute(sql, params_list)
+            result_set = cursor.fetchall()
+            return result_set
+        except sqlite3.DatabaseError, db_error:
+            print db_error.message
+            return None
+        finally:
+            if cursor is not None:
+                cursor.close()
+            if is_main_thread is False and connect is not None:
+                connect.close()
