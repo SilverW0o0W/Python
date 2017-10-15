cbf0cc223e4595aea671672db818c8e23fccb26a
Silver <silver.codingcat@gmail.com>
2017-10-15 23:53:04 +0800

    Add log method

--- Code/netease-music/logging_controller.py
+++ Code/netease-music/logging_controller.py
@@ -7,6 +7,15 @@ import logging
 import logging.config
 import cloghandler
 
+CRITICAL = 50
+FATAL = CRITICAL
+ERROR = 40
+WARNING = 30
+WARN = WARNING
+INFO = 20
+DEBUG = 10
+NOTSET = 0
+
 
 class LoggingController(object):
     """
@@ -18,17 +27,63 @@ class LoggingController(object):
         logging.config.dictConfig(config_value)
         self.pipe = Pipe(duplex=False)
         self.logger_name = logger_name
-        self.dispose = False
-        log_process = Process(target=self.logger_process,
+        log_process = Process(target=self._logger_process,
                               args=(self.pipe[0], self.logger_name,))
         log_process.start()
 
-    def logger_process(self, pipe, logger_name):
+    def _logger_process(self, pipe, logger_name):
         logger = logging.getLogger(
             logger_name) if logger_name else logging.getLogger()
-        self.print_log(logger, pipe)
+        self._print_log(logger, pipe)
 
-    def print_log(self, logger, pipe):
+    def _print_log(self, logger, pipe):
         while True:
-            if self.dispose:
-                break
+            key_value = pipe.recv()
+
+    def setLevel(self, level):
+        """
+        Set the logging level of this logger.
+        """
+        pass
+
+    def debug(self, msg, *args, **kwargs):
+        """
+        Debug level
+        """
+        self.log(DEBUG, msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        """
+        Info level
+        """
+        self.log(INFO, msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        """
+        Warning level
+        """
+        self.log(WARNING, msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        """
+        Error level
+        """
+        self.log(ERROR, msg, *args, **kwargs)
+
+    def exception(self, msg, *args, **kwargs):
+        """
+        Exception level
+        """
+        kwargs['exc_info'] = 1
+        self.error(msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        """
+        Critical level
+        """
+        self.log(CRITICAL, msg, *args, **kwargs)
+
+    def log(self, level, msg, *args, **kwargs):
+        """
+        Logger level
+        """
