91581a308b58925c630731491d21110f96285e1f
Silver <silver.codingcat@gmail.com>
2017-12-18 22:35:32 +0800

    Add migreate model

--- Code/django/HelloWorld/HelloWorld/settings.py
+++ Code/django/HelloWorld/HelloWorld/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_1st.apps.App1StConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Code/django/HelloWorld/app_1st/models.py
+++ Code/django/HelloWorld/app_1st/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class FirstClass(models.Model):
+    class_name = models.CharField(max_length=100)
+    create_date = models.DateTimeField('date published')
+
+
+class SecondClass(models.Model):
+    first = models.ForeignKey(FirstClass, on_delete=models.CASCADE)
+    second_text = models.CharField(max_length=200)
+    count = models.IntegerField(default=0)
