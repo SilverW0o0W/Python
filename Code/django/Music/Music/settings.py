6a20872a60e3e4f4717dc38e18dca10132d7264d
Silver <silver.codingcat@gmail.com>
2018-02-12 22:19:41 +0800

    Update music site

--- Code/django/Music/Music/settings.py
+++ Code/django/Music/Music/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lyric_exporter.apps.LyricExporterConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
--- Code/django/Music/lyric_exporter/models.py
+++ Code/django/Music/lyric_exporter/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Lyric(models.Model):
+    song_id = models.CharField(max_length=12)
+    last_modified = models.DateField('date published')
+    locate_path = models.CharField(max_length=300)
