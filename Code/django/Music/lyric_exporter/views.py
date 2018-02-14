00362cccb07a57cc9f1beff1aea03faf9dfc5f7c
Silver <silver.codingcat@gmail.com>
2018-02-14 23:58:31 +0800

    Update exporter

--- Code/django/Music/lyric_exporter/views.py
+++ Code/django/Music/lyric_exporter/views.py
@@ -3,5 +3,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("First Page of App Lyric Exporter")
+    context = {
+        'welcome': "First Page of App Lyric Exporter",
+    }
+    return render(request, 'lyric_exporter/index.html', context)
+
+def export(request):
+    return render(request,'lyric_exporter/index.html')
