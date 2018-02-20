37fe8c210d13da0cbfbd8fc46545898fe62d182a
Silver <silver.codingcat@gmail.com>
2018-02-20 18:07:31 +0800

    Add export playlist

--- Code/django/Music/lyric_exporter/models.py
+++ Code/django/Music/lyric_exporter/models.py
@@ -2,10 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 
 
 class Lyric(models.Model):
-    song_id = models.CharField(max_length=12)
-    last_modified = models.DateField('date published')
+    song_id = models.CharField(primary_key=True, max_length=12)
+    last_modified = models.DateField(auto_now=True)
     locate_path = models.CharField(max_length=300)
--- Code/django/Music/lyric_exporter/views.py
+++ Code/django/Music/lyric_exporter/views.py
@@ -1,19 +1,24 @@
+# coding=utf-8
 from django.shortcuts import render
 from django.http import FileResponse
 
 from models import Lyric
 from lyric_exporter import LyricExporter
 import spider.music_utils as utils
+import zipfile
+import os
 
 
 # Create your views here.
-
 
 def index(request):
     context = {
         'welcome': "First Page of App Lyric Exporter",
     }
     return render(request, 'lyric_exporter/index.html', context)
+
+
+download_path = 'lyric_exporter/download'
 
 
 def export_action(request):
@@ -23,7 +28,7 @@ def export_action(request):
             'welcome': "First Page of App Lyric Exporter",
         }
         return render(request, 'lyric_exporter/index.html', context)
-    type = request.POST.get('type', 'song')
+    url_type = request.POST.get('type', 'song')
     format = request.POST.get('format', '1')
     format_dict = {
         '1': '{1}',
@@ -31,30 +36,43 @@ def export_action(request):
         '3': '{1} - {0}',
     }
     name_format = format_dict[format]
-    exporter = LyricExporter('lyric_exporter/download/lyric/', name_format=name_format)
-    path = export_song(exporter, url) if type == 'song' else export_playlist(exporter, url)
-    file = open(path, 'rb')
+    lyric_dir = str.format('{0}/lyric/', download_path)
+    exporter = LyricExporter(lyric_dir, name_format=name_format)
+    file_name = export_song(exporter, url) if url_type == 'song' else export_playlist(exporter, url)
+    file = open(file_name[1], 'rb')
     response = FileResponse(file)
     response['Content-Type'] = 'application/octet-stream'
-    response['Content-Disposition'] = 'attachment;filename="test.lrc'
+    response['Content-Disposition'] = str.format('attachment;filename={0}', file_name[0])
     return response
 
 
 def export_song(exporter, url):
     song_id = utils.match_song_id(url)
-    path = exporter.export(song_id)
+    file_name = exporter.export(song_id)
     lyric = Lyric()
-    lyric.song_id = id
-    lyric.locate_path = path
+    lyric.song_id = song_id
+    lyric.locate_path = file_name[1]
     lyric.save()
-    return path
+    return file_name
 
 
 def export_playlist(exporter, url):
     playlist_id = utils.match_playlist_id(url)
     path_dict = exporter.export_playlist(playlist_id)
-    for path in path_dict:
-        lyric = Lyric()
-        lyric.song_id = path
-        lyric.locate_path = path
-        lyric.save()
+    zip_name = str.format('{0}.zip', playlist_id)
+    playlist_dir = str.format('{0}/playlist', download_path)
+    zip_real_name = str.format('{0}/{1}', playlist_dir, zip_name)
+
+    if not os.path.isdir(playlist_dir):
+        os.makedirs(playlist_dir)
+
+    with zipfile.ZipFile(zip_real_name, 'w') as zip_file:
+        for song_id, file_name in path_dict.items():
+            lyric = Lyric()
+            lyric.song_id = song_id
+            lyric.locate_path = file_name[1]
+            lyric.save()
+            short_name = file_name[0].decode('utf-8')
+            long_name = file_name[1].decode('utf-8')
+            zip_file.write(long_name, short_name, compress_type=zipfile.ZIP_DEFLATED)
+    return zip_name, zip_real_name
