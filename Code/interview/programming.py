d8d45b143e1b97bd4081287f199a5363fcc41bbc
Silver <silver.codingcat@gmail.com>
2018-03-29 23:57:00 +0800

    Add merge two sort list

--- Code/interview/programming.py
+++ Code/interview/programming.py
@@ -84,3 +84,23 @@ dict1 = dict((['name', 'earth'], ['port', '80']))
 # 3
 dict1 = {}.fromkeys(('x', 'y'), -1)
 dict2 = {}.fromkeys(('x', 'y'))
+
+
+# Merge two sort list
+def _recursion_merge_sort(l1, l2, tmp):
+    if len(l1) == 0 or len(l2) == 0:
+        tmp.extend(l1)
+        tmp.extend(l2)
+        return tmp
+    else:
+        if l1[0] < l2[0]:
+            tmp.append(l1[0])
+            del l1[0]
+        else:
+            tmp.append(l2[0])
+            del l2[0]
+        return recursion_merge_sort(l1, l2, tmp)
+
+
+def recursion_merge_sort(l1, l2):
+    return _recursion_merge_sort(l1, l2, [])
