64fc674d60912c47768741cf8c6e2dbcafb1158e
lizongxin <silver.codingcat@gmail.com>
2022-09-29 12:50:47 +0800

    Update singleton_1.py

--- Code/interview/singleton/singleton_1.py
+++ Code/interview/singleton/singleton_1.py
@@ -14,6 +14,15 @@ class Singleton(object):
         return cls._instance
 
 
+class Singleton2(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
 class Test(Singleton):
     def __init__(self, index):
         self.index = index
